6c43da2f041cc96b4186d3918af63a69f80e7ddf
SharanbabuB <sharanbabu545@gmail.com>
2025-07-05 23:04:12 +0530

    Delete Superset ID_6390124_HandsOn directory

--- Superset ID_6390124_HandsOn/Week3_Spring Core and Maven Exercises/LibraryManagement/src/main/java/com/library/repository/BookRepository2.java
+++
@@ -1,7 +0,0 @@
-package com.library.repository;
-
-public class BookRepository2 {
-    public void saveBook(String title) {
-        System.out.println("BookRepository2: Book saved - " + title);
-    }
-}
--- Superset ID_6390124_HandsOn/Week3_Spring Core and Maven Exercises/LibraryManagement/src/main/java/com/library/repository/BookRepository3.java
+++
@@ -1,7 +0,0 @@
-package com.library.repository;
-
-public class BookRepository3 {
-    public void saveBook(String title) {
-        System.out.println("BookRepository3: Book saved - " + title);
-    }
-}
